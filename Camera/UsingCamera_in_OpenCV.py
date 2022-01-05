d9a07ccc03057031a130a7c1e90b3762ad4d5c07
waveisme <hoangthixuanquynh130701@gmail.com>
2022-01-05 14:48:50 +0700

    update

---
+++ Camera/UsingCamera_in_OpenCV.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+
+#                          License Agreement
+#                         3-clause BSD License
+#
+#       Copyright (C) 2018, Xperience.AI, all rights reserved.
+#
+# Third party copyrights are property of their respective owners.
+#
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+#
+#   * Redistributions of source code must retain the above copyright notice,
+#     this list of conditions and the following disclaimer.
+#
+#   * Redistributions in binary form must reproduce the above copyright notice,
+#     this list of conditions and the following disclaimer in the documentation
+#     and/or other materials provided with the distribution.
+#
+#   * Neither the names of the copyright holders nor the names of the contributors
+#     may be used to endorse or promote products derived from this software
+#     without specific prior written permission.
+#
+# This software is provided by the copyright holders and contributors "as is" and
+# any express or implied warranties, including, but not limited to, the implied
+# warranties of merchantability and fitness for a particular purpose are disclaimed.
+# In no event shall copyright holders or contributors be liable for any direct,
+# indirect, incidental, special, exemplary, or consequential damages
+# (including, but not limited to, procurement of substitute goods or services;
+# loss of use, data, or profits; or business interruption) however caused
+# and on any theory of liability, whether in contract, strict liability,
+# or tort (including negligence or otherwise) arising in any way out of
+# the use of this software, even if advised of the possibility of such damage.
+#import the opencv library
+import cv2
+import sys
+
+s = 0
+if len(sys.argv) > 1:
+    s = sys.argv[1]
+#define a video capture object
+#if s = 0: call default camera, 1 camera, 2 camera 2
+
+source = cv2.VideoCapture(s)
+
+win_name = 'Camera Preview'
+cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+
+while cv2.waitKey(1) != 27: # Escape
+    #Capture the video frame
+    has_frame, frame = source.read()
+    if not has_frame:
+        break
+    #Display the resulting frame
+    cv2.imshow(win_name, frame)
+#After the loop release the cap object
+source.release()
+#Destroy all the windows
+cv2.destroyWindow(win_name)
